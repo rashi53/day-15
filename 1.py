2347487f6329e28441f957463d2485a424a4707e
rashi53 <51444233+rashi53@users.noreply.github.com>
2019-07-03 19:21:23 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,69 @@
+import random
+import math
+import operator
+def getResponse(neighbors):
+    classVotes={}
+    for x in range(len(neighbors)):
+        response=neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response]+=1
+        else:
+            classVotes[response]=1
+        sortedVotes=sorted(classVotes.items(),key=operator.itemgetter(1),reverse=True)
+        return sortedVotes[0][0]
+
+
+def getNeighbors(trainingSet,testInstance,k):
+    distances=[]
+    length=4
+    for x in range(len(trainingSet)):
+        dist=euclideanDistance(testInstance,trainingSet[x],length)
+        distances.append((trainingSet[x],dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors=[]
+    for x in range(k):
+                   neighbors.append(distances[x][0])
+    return neighbors
+    
+def convert_str_to_float(x,n):
+    for i in range(n):
+        x[i]=float(x[i])
+    return x
+
+def load_split_data(split,training_data=[],test_data=[]):
+    for line in open("iris.txt","r"):
+        temp = convert_str_to_float(line[0:-1].split(','),4)
+        if random.random()<=split:
+            training_data.append(temp)
+        else:
+            test_data.append(temp)
+    return(training_data,test_data)
+def euclideanDistance(instance1,instance2,length):
+    distance=0
+    for x in range(length):
+        distance+=pow((instance1[x]-instance2[x]),2)
+    return math.sqrt(distance)
+
+def getAccuracy(testSet,predictions):
+    correct=0
+    for x in range(len(testSet)):
+        if testSet[x][-1]==predictions[x]:
+            correct+=1
+    return(correct/float(len(testSet)))*100.0
+def main():
+    trainingSet=[]
+    testSet=[]
+    split=0.70
+    trainingSet,testSet=load_split_data(split)
+    print('trainingSet:'+str(len(trainingSet)))
+    print('testSet:'+str(len(testSet)))
+    predictions=[]
+    k=3
+    for x in range(len(testSet)):
+        neighbors=getNeighbors(trainingSet,testSet[x],k)
+        result=getResponse(neighbors)
+        predictions.append(result)
+        print('>predicted='+str(result)+',actual='+str(testSet[x][-1]))
+    accuracy=getAccuracy(testSet,predictions)
+    print('Accuracy:'+str(accuracy)+'%')
+main()
